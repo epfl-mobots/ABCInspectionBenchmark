05abb555da308b2f1a931e0b23430e25ef2e53b5
sirogo <cyril.sirogo@gmail.com>
2024-07-12 11:35:56 +0200

    corrected addr for RPi

--- DP832A_connection_test.py
+++ DP832A_connection_test.py
@@ -2,13 +2,15 @@ import pyvisa
 
 # Initialisation de la ressource manager VISA
 rm = pyvisa.ResourceManager('@py')
+#rm = pyvisa.ResourceManager() # To use the default VISA resource manager
 print("Resource Manager:", rm)
 
 # Liste des ressources disponibles
 resources = rm.list_resources()
 print("Available Resources:", resources)
 
-DP832A_addr = 'USB0::0x1AB1::0x0E11::DP8B212300503::INSTR'
+#DP832A_addr = 'USB0::0x1AB1::0x0E11::DP8B212300503::INSTR' # On Mac OS
+DP832A_addr = 'USB0::6833::3601::DP8B212300503::0::INSTR' # On RPi
 
 if DP832A_addr in resources:
     DCps = rm.open_resource(DP832A_addr) # Connection to the DC power supply
