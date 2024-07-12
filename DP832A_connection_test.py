4343b6ede2de71ddfddf28a2fd51847ba217f892
sirogo <cyril.sirogo@gmail.com>
2024-07-12 10:16:50 +0200

    added debug msgs

--- DP832A_connection_test.py
+++ DP832A_connection_test.py
@@ -2,15 +2,21 @@ import pyvisa
 
 # Initialisation de la ressource manager VISA
 rm = pyvisa.ResourceManager('@py')
+print("Resource Manager:", rm)
 
 # Liste des ressources disponibles
-print(rm.list_resources())
+resources = rm.list_resources()
+print("Available Resources:", resources)
 
 DP832A_addr = 'USB0::0x1AB1::0x0E11::DP8B212300503::INSTR'
-DCps = rm.open_resource(DP832A_addr) # Connection to the DC power supply
 
-# Configuration de la source DC (exemple : identification de l'instrument)
-print(DCps.query('*IDN?'))
+if DP832A_addr in resources:
+    DCps = rm.open_resource(DP832A_addr) # Connection to the DC power supply
+    
+    # Configuration de la source DC (exemple : identification de l'instrument)
+    print(DCps.query('*IDN?'))
 
-# Fermer la connexion
-DCps.close()
+    # Fermer la connexion
+    DCps.close()
+else:
+    print("Device not found in available resources")
